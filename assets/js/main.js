c4ff881fe213f36bbaf3dae8e2a4122df3ab4636
VISHTECHSOFTWARESGIT <vishtechsoftwareservices@gmail.com>
2025-07-31 01:59:26 +0530

    lastupdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 
+ fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    });
 
+     fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    });
 
 
 document.addEventListener("DOMContentLoaded", function() {
